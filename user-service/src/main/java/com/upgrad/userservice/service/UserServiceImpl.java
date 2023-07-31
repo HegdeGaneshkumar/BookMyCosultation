965080679e2196f089ff5117262d6f80ab889373
Ganesh <hegdeganeshkumar@gmail.com>
2023-07-31 22:53:02 +0530

    Notification, email, s3 done

--- user-service/src/main/java/com/upgrad/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/com/upgrad/userservice/service/UserServiceImpl.java
@@ -3,9 +3,11 @@ package com.upgrad.userservice.service;
 import com.upgrad.userservice.dao.UserRepository;
 import com.upgrad.userservice.entities.UserInfoEntity;
 import com.upgrad.userservice.exception.RequestedResourceUnavailable;
+import com.upgrad.userservice.producer.KafkaMessageProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
 @Service
@@ -14,11 +16,21 @@ public class UserServiceImpl implements UserService{
     @Autowired
     UserRepository userRepository;
 
+    private static final String TOPIC_NAME = "user-service";
+
+    @Autowired
+    private KafkaMessageProducer kafkaMessageProducer ;
+
     @Override
     public UserInfoEntity saveUserDetails(UserInfoEntity userInfoEntity) {
         userInfoEntity.setCreatedDate(LocalDate.now());
         UserInfoEntity savedUser = userRepository.save(userInfoEntity);
-
+        try {
+            kafkaMessageProducer.publish(TOPIC_NAME, savedUser.getEmailId(), savedUser.toString());
+        }
+        catch (IOException exception){
+            System.out.println(exception);
+        }
         return savedUser;
     }
 
